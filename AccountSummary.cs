25741704478730e7df5d73b289784be0dca1fa2c
ProfessorX0227 <Zavier2742@yahoo.com>
2020-03-01 16:28:59 -0500

    added account balance display class

---
+++ AccountSummary.cs
@@ -0,0 +1,28 @@
+﻿using Alpaca.Markets;
+using System;
+using System.Threading.Tasks;
+
+namespace MarketDataMonitorAPI
+{
+     public class AccountSummary
+     {
+        private static string API_KEY = "PK43DG0LFRIX11TF9LF8";
+
+        private static string API_SECRET = "VbqwOhfIf8HWU5XngW/mp40A7kOhcHkeg4Km7CcL";
+
+        public async Task ExecuteBalanceView()
+        {
+           // First, open the API connection
+            var client = Environments.Paper
+                .GetAlpacaTradingClient(API_KEY, new SecretKey(API_SECRET));
+
+            // Get account info
+            var account = await client.GetAccountAsync();
+
+            //# Check our current balance vs. our balance at the last market close
+            var balance_change = account.Equity - account.LastEquity;
+            Console.WriteLine($"Today's portfolio balance change: ${balance_change}");
+
+        }
+     }
+}
--- Program.cs
+++ Program.cs
@@ -12,10 +12,10 @@ namespace Example
             SMADayAverage RealTimeSMAAverage = new SMADayAverage();
 
             //calls method from SMADayAverage class
-           var SMAAverageDay_20 = RealTimeSMAAverage.SMACurrentAverage(20);
-           var SMAAverageDay_10 = RealTimeSMAAverage.SMACurrentAverage(10);
-           var SMAAverageDay_5 = RealTimeSMAAverage.SMACurrentAverage(5);
-            
+            var SMAAverageDay_20 = RealTimeSMAAverage.SMACurrentAverage(20);
+            var SMAAverageDay_10 = RealTimeSMAAverage.SMACurrentAverage(10);
+            var SMAAverageDay_5 = RealTimeSMAAverage.SMACurrentAverage(5);
+
             Console.WriteLine($"20SMA: { SMAAverageDay_20}");
             Console.WriteLine($"10SMA: {SMAAverageDay_10}");
             Console.WriteLine($"5SMA: {SMAAverageDay_5}");
@@ -32,13 +32,15 @@ namespace Example
             BuyShares buy = new BuyShares();
             var buyTicker = buy.ExecuteOrder("AAPL");
 
-            Console.WriteLine(buyTicker);
-
             //initializes method from SellShares class and then executes sell order for ticker
             SellShares sell = new SellShares();
             var sellTicker = sell.ExecuteOrder("AAPL");
 
-            Console.WriteLine(sellTicker);
+            //initializes method from Account Summary class then displays gain/loss on account for the intraday
+            AccountSummary ViewAccountSummary = new AccountSummary();
+            var portfolioBalance = ViewAccountSummary.ExecuteBalanceView();
+            Console.WriteLine(portfolioBalance);
+
 
             Console.ReadLine();
 
